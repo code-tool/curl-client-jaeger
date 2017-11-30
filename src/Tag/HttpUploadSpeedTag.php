5c88e0edaa07b4c94c9869a01fb45d8ab41dbc25
allflame <allflame@syneforge.com>
2017-11-30 15:03:10 +0200

    Make speed tags float

--- src/Tag/HttpUploadSpeedTag.php
+++ src/Tag/HttpUploadSpeedTag.php
@@ -3,11 +3,12 @@ declare(strict_types=1);
 
 namespace Http\Client\Curl\Jaeger\Tag;
 
+use Jaeger\Tag\DoubleTag;
 use Jaeger\Tag\LongTag;
 
-class HttpUploadSpeedTag extends LongTag
+class HttpUploadSpeedTag extends DoubleTag
 {
-    public function __construct(int $value)
+    public function __construct(float $value)
     {
         parent::__construct('http.upload_speed', $value);
     }
